aae3ec333d8accf695715dc105b7e4671f8c1df5
obidar <omidbidar4@gmail.com>
2021-01-22 16:25:26 +0000

    Added kOmegaSSTfieldInversion files in src/adjoint/model and added related code in make files

---
+++ src/adjoint/models/kOmegaSSTfieldInversion/kOmegaSSTfieldInversion.C
@@ -0,0 +1,116 @@
+/*---------------------------------------------------------------------------*\
+
+    DAFoam  : Discrete Adjoint with OpenFOAM
+    Version : v2
+
+\*---------------------------------------------------------------------------*/
+
+#include "kOmegaSSTfieldInversion.H"
+#include "fvOptions.H"
+#include "bound.H"
+#include "wallDist.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace RASModels
+{
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+void kOmegaSSTfieldInversion<BasicTurbulenceModel>::correctNut()
+{
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+kOmegaSSTfieldInversion<BasicTurbulenceModel>::kOmegaSSTfieldInversion(
+    const alphaField& alpha,
+    const rhoField& rho,
+    const volVectorField& U,
+    const surfaceScalarField& alphaRhoPhi,
+    const surfaceScalarField& phi,
+    const transportModel& transport,
+    const word& propertiesName,
+    const word& type)
+    : eddyViscosity<RASModel<BasicTurbulenceModel>>(
+        type,
+        alpha,
+        rho,
+        U,
+        alphaRhoPhi,
+        phi,
+        transport,
+        propertiesName),
+      omega_(
+          IOobject(
+              "omega",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      betaFieldInversion_(
+          IOobject(
+              "betaFieldInversion",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      betaPrior_(
+          IOobject(
+              "betaPrior",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      UTrue_(
+          IOobject(
+              "UTrue",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      y_(wallDist::New(this->mesh_).y())
+
+{
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+bool kOmegaSSTfieldInversion<BasicTurbulenceModel>::read()
+{
+
+    return true;
+}
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kOmegaSSTfieldInversion<BasicTurbulenceModel>::k() const
+{
+    return this->nut_;
+}
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kOmegaSSTfieldInversion<BasicTurbulenceModel>::epsilon() const
+{
+    return this->nut_;
+}
+
+template<class BasicTurbulenceModel>
+void kOmegaSSTfieldInversion<BasicTurbulenceModel>::correct()
+{
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace RASModels
+} // End namespace Foam
+
+// ************************************************************************* //
