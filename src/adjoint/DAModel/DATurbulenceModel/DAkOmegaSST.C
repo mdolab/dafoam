159d62f7174386e46da0037f2be3e9fa0624dfb8
Ping He <phe@iastate.edu>
2021-05-06 18:17:20 -0500

    Removed unused variables for SSTLM.

--- src/adjoint/DAModel/DATurbulenceModel/DAkOmegaSST.C
+++ src/adjoint/DAModel/DATurbulenceModel/DAkOmegaSST.C
@@ -92,14 +92,6 @@ DAkOmegaSST::DAkOmegaSST(
           dimensionedScalar("omegaRes", dimensionSet(0, 0, -2, 0, 0, 0, 0), 0.0),
 #endif
           zeroGradientFvPatchField<scalar>::typeName),
-      omegaResRef_(
-          IOobject(
-              "omegaResRef",
-              mesh.time().timeName(),
-              mesh,
-              IOobject::NO_READ,
-              IOobject::NO_WRITE),
-          omegaRes_),
       omegaResPartDeriv_(
           IOobject(
               "omegaResPartDeriv",
@@ -108,14 +100,6 @@ DAkOmegaSST::DAkOmegaSST(
               IOobject::NO_READ,
               IOobject::NO_WRITE),
           omegaRes_),
-      omegaRef_(
-          IOobject(
-              "omegaRef",
-              mesh.time().timeName(),
-              mesh,
-              IOobject::NO_READ,
-              IOobject::NO_WRITE),
-          omega_),
       k_(
           const_cast<volScalarField&>(
               mesh_.thisDb().lookupObject<volScalarField>("k"))),
@@ -134,14 +118,6 @@ DAkOmegaSST::DAkOmegaSST(
           dimensionedScalar("kRes", dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0),
 #endif
           zeroGradientFvPatchField<scalar>::typeName),
-      kResRef_(
-          IOobject(
-              "kResRef",
-              mesh.time().timeName(),
-              mesh,
-              IOobject::NO_READ,
-              IOobject::NO_WRITE),
-          kRes_),
       kResPartDeriv_(
           IOobject(
               "kResPartDeriv",
@@ -150,14 +126,6 @@ DAkOmegaSST::DAkOmegaSST(
               IOobject::NO_READ,
               IOobject::NO_WRITE),
           kRes_),
-      kRef_(
-          IOobject(
-              "kRef",
-              mesh.time().timeName(),
-              mesh,
-              IOobject::NO_READ,
-              IOobject::NO_WRITE),
-          k_),
       y_(mesh_.thisDb().lookupObject<volScalarField>("yWall"))
 {
 
