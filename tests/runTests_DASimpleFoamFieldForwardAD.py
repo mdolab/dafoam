22295d3b8993d4e18b16578707ff76b7d13c2170
obidar <omidbidar4@gmail.com>
2021-12-01 20:51:01 +0000

    Update runTests_DASimpleFoamFieldForwardAD.py

--- tests/runTests_DASimpleFoamFieldForwardAD.py
+++ tests/runTests_DASimpleFoamFieldForwardAD.py
@@ -15,7 +15,7 @@ from testFuncs import *
 
 gcomm = MPI.COMM_WORLD
 
-os.chdir("./input/NACA0012betaSA")
+os.chdir("./input/NACA0012BetaSA")
 
 replace_text_in_file("constant/turbulenceProperties", "RASModel             SpalartAllmarasFv3Beta;", "RASModel             SpalartAllmarasFv3FieldInversion;")
 
