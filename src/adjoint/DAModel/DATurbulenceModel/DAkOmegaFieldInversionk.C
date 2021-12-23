99c8640c2e4a2892516da0791e4e6865be450e77
Ping He <phe@iastate.edu>
2021-12-23 16:51:21 -0600

    Added OpenFOAM's copyright and license header to DAFoam files that have significant overlap with OpenFOAM's original source codes.

--- src/adjoint/DAModel/DATurbulenceModel/DAkOmegaFieldInversionk.C
+++ src/adjoint/DAModel/DATurbulenceModel/DAkOmegaFieldInversionk.C
@@ -3,6 +3,28 @@
     DAFoam  : Discrete Adjoint with OpenFOAM
     Version : v2
 
+    This file is modified from OpenFOAM's source code
+    src/TurbulenceModels/turbulenceModels/RAS/kOmega/kOmega.C
+
+    OpenFOAM: The Open Source CFD Toolbox
+
+    Copyright (C): 2011-2016 OpenFOAM Foundation
+
+    OpenFOAM License:
+
+        OpenFOAM is free software: you can redistribute it and/or modify it
+        under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+    
+        OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+        ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+        FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+        for more details.
+    
+        You should have received a copy of the GNU General Public License
+        along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
 \*---------------------------------------------------------------------------*/
 
 #include "DAkOmegaFieldInversionk.H"
