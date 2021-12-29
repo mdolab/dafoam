d59175c95c0abdf78ae364bce49799b6e827b5c0
Ping He <phe@iastate.edu>
2021-12-29 10:19:23 -0600

    Reduced the required mpi4py version to 3.0.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     ],
     install_requires=[
         "numpy>=1.16.4",
-        "mpi4py>=3.0.2",
+        "mpi4py>=3.0.0",
         "petsc4py>=3.11.0",
         "cython>=0.29.21",
     ],
