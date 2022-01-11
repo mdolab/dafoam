a46d32b0fad92af0b4aab8d4b6865a8cf46c348b
obidar <omidbidar4@gmail.com>
2022-01-11 20:53:10 +0000

    Added all the FI fields for the kOmegaFI models

--- src/adjoint/models/kOmegaFieldInversionOmega/kOmegaFieldInversionOmega.C
+++ src/adjoint/models/kOmegaFieldInversionOmega/kOmegaFieldInversionOmega.C
@@ -85,6 +85,36 @@ kOmegaFieldInversionOmega<BasicTurbulenceModel>::kOmegaFieldInversionOmega(
               IOobject::MUST_READ,
               IOobject::AUTO_WRITE),
           this->mesh_),
+      surfaceFriction_(
+          IOobject(
+              "surfaceFriction",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("surfaceFriction", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
+      surfaceFrictionRef_(
+          IOobject(
+              "surfaceFrictionRef",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("surfaceFrictionRef", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
+      surfacePressureRef_(
+          IOobject(
+              "surfacePressureRef",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("surfacePressureRef", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
       profileRefFieldInversion_(
           IOobject(
               "profileRefFieldInversion",
