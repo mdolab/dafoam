3d4486087e0ccd75a8b4acc2b79f6fb7271475c6
Ping He <phe@iastate.edu>
2022-05-12 10:10:11 -0500

    Added a member func.

--- src/adjoint/DAFPAdj/DAFPAdjSimpleFoam.C
+++ src/adjoint/DAFPAdj/DAFPAdjSimpleFoam.C
@@ -54,6 +54,62 @@ label DAFPAdjSimpleFoam::run(
     return 0;
 }
 
+void DAFPAdjSimpleFoam::invTranProd_UEqn(
+    const List<vector>& mySource,
+    volVectorField& pseudo_U)
+{
+    /*
+    Description:
+        Describe what this function does
+    */
+
+    const objectRegistry& db = mesh_.thisDb();
+    const volVectorField& U = db.lookupObject<volVectorField>("U");
+    const volScalarField& p = db.lookupObject<volScalarField>("p");
+    const surfaceScalarField& phi = db.lookupObject<surfaceScalarField>("phi");
+    const DATurbulenceModel& daTurb = daModel_.getDATurbulenceModel();
+    volScalarField nuEff = daTurb.nuEff();
+
+    // Get the pseudo_UEqn,
+    // the most important thing here is to make sure the l.h.s. matches that of UEqn.
+    //pseudo_U = U;
+    fvVectorMatrix pseudo_UEqn(
+        fvm::div(phi, pseudo_U)
+        - fvm::laplacian(nuEff, pseudo_U)
+        - fvc::div(nuEff * dev2(T(fvc::grad(pseudo_U)))));
+    pseudo_UEqn.relax();
+
+    // Swap upper() and lower()
+    List<scalar> temp = pseudo_UEqn.upper();
+    pseudo_UEqn.upper() = pseudo_UEqn.lower();
+    pseudo_UEqn.lower() = temp;
+
+    // Overwrite the r.h.s.
+    pseudo_UEqn.source() = mySource;
+
+    // Make sure that boundary contribution to source is zero,
+    // Alternatively, we can deduct source by boundary contribution, so that it would cancel out during solve.
+    forAll(pseudo_U.boundaryField(), patchI)
+    {
+        const fvPatch& pp = pseudo_U.boundaryField()[patchI].patch();
+        forAll(pp, faceI)
+        {
+            label cellI = pp.faceCells()[faceI];
+            pseudo_UEqn.source()[cellI] -= pseudo_UEqn.boundaryCoeffs()[patchI][faceI];
+        }
+    }
+
+    // Before solve, force xEqn.psi() to be solved into all zero
+    forAll(pseudo_U.primitiveFieldRef(), cellI)
+    {
+        pseudo_U.primitiveFieldRef()[cellI][0] = 0;
+        pseudo_U.primitiveFieldRef()[cellI][1] = 0;
+        pseudo_U.primitiveFieldRef()[cellI][2] = 0;
+    }
+
+    pseudo_UEqn.solve();
+}
+
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 } // End namespace Foam
