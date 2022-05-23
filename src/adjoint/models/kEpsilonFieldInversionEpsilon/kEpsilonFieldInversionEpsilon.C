fe729a052b3891bdbf55ece3fd3944e93779ff31
obidar <omidbidar4@gmail.com>
2022-05-23 15:48:42 +0100

    added k-epsilon field inversion with modification to the epsilon equation

---
+++ src/adjoint/models/kEpsilonFieldInversionEpsilon/kEpsilonFieldInversionEpsilon.C
@@ -0,0 +1,164 @@
+/*---------------------------------------------------------------------------*\
+
+    DAFoam  : Discrete Adjoint with OpenFOAM
+    Version : v3
+
+\*---------------------------------------------------------------------------*/
+
+#include "kEpsilonFieldInversionEpsilon.H"
+#include "fvOptions.H"
+#include "bound.H"
+#include "wallDist.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace RASModels
+{
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+void kEpsilonFieldInversionEpsilon<BasicTurbulenceModel>::correctNut()
+{
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+kEpsilonFieldInversionEpsilon<BasicTurbulenceModel>::kEpsilonFieldInversionEpsilon(
+    const alphaField& alpha,
+    const rhoField& rho,
+    const volVectorField& U,
+    const surfaceScalarField& alphaRhoPhi,
+    const surfaceScalarField& phi,
+    const transportModel& transport,
+    const word& propertiesName,
+    const word& type)
+    : eddyViscosity<RASModel<BasicTurbulenceModel>>(
+        type,
+        alpha,
+        rho,
+        U,
+        alphaRhoPhi,
+        phi,
+        transport,
+        propertiesName),
+      omega_(
+          IOobject(
+              "omega",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      k_(
+          IOobject(
+              "k",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      betaFieldInversion_(
+          IOobject(
+              "betaFieldInversion",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      betaRefFieldInversion_(
+          IOobject(
+              "betaRefFieldInversion",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),
+          this->mesh_),
+      varRefFieldInversion_(
+          IOobject(
+              "varRefFieldInversion",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::MUST_READ,
+              IOobject::AUTO_WRITE),              
+          this->mesh_),
+      surfaceFriction_(
+          IOobject(
+              "surfaceFriction",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("surfaceFriction", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
+      surfaceFrictionRef_(
+          IOobject(
+              "surfaceFrictionRef",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("surfaceFrictionRef", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
+      surfacePressureRef_(
+          IOobject(
+              "surfacePressureRef",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("surfacePressureRef", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
+      profileRefFieldInversion_(
+          IOobject(
+              "profileRefFieldInversion",
+              this->runTime_.timeName(),
+              this->mesh_,
+              IOobject::READ_IF_PRESENT,
+              IOobject::AUTO_WRITE),
+          this->mesh_,
+          dimensionedScalar("profileRefFieldInversion", dimensionSet(0, 0, 0, 0, 0, 0, 0), 0.0),
+          zeroGradientFvPatchField<scalar>::typeName),
+      y_(wallDist::New(this->mesh_).y())
+
+{
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+bool kEpsilonFieldInversionEpsilon<BasicTurbulenceModel>::read()
+{
+
+    return true;
+}
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kEpsilonFieldInversionEpsilon<BasicTurbulenceModel>::k() const
+{
+    return this->nut_;
+}
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kEpsilonFieldInversionEpsilon<BasicTurbulenceModel>::epsilon() const
+{
+    return this->nut_;
+}
+
+template<class BasicTurbulenceModel>
+void kEpsilonFieldInversionEpsilon<BasicTurbulenceModel>::correct()
+{
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace RASModels
+} // End namespace Foam
+
+// ************************************************************************* //
