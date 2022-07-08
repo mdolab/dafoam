b48f02cfa63945e29a90cb2cc10112b81df75c28
Lean Fang <leanfang@iastate.edu>
2022-07-08 16:24:49 -0500

    WiP

--- src/adjoint/DAModel/DATurbulenceModel/DASpalartAllmarasFv3.C
+++ src/adjoint/DAModel/DATurbulenceModel/DASpalartAllmarasFv3.C
@@ -96,6 +96,10 @@ DASpalartAllmarasFv3::DASpalartAllmarasFv3(
           dimensionedScalar("nuTildaRes", dimensionSet(0, 2, -2, 0, 0, 0, 0), 0.0),
 #endif
           zeroGradientFvPatchField<scalar>::typeName),
+      // pseudoNuTilda_ and pseudoNuTildaEqn_ for solving adjoint equation
+      volScalarField pseudoNuTilda_("pseudoNuTilda_", nuTilda_),
+      fvScalarMatrix pseudoNuTildaEqn_(
+          fvm::ddt(nuTilda_)),
       y_(mesh.thisDb().lookupObject<volScalarField>("yWall"))
 {
 
