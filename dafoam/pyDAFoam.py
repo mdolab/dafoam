23d3812c4d1978454b43c8f7b19bc5b356bea2a3
Ping He <phe@iastate.edu>
2022-08-27 16:31:57 -0500

    Updated the tests and deprecate the writeDeformedFFDs call.

--- dafoam/pyDAFoam.py
+++ dafoam/pyDAFoam.py
@@ -1174,11 +1174,17 @@ class PYDAFOAM(object):
         """
         Write the deformed FFDs to the disk during optimization
         """
+
+        if self.comm.rank == 0:
+            print("writeDeformedFFDs is deprecated since v3.0.1!")
+
+        """
         if self.getOption("writeDeformedFFDs"):
             if counter is None:
                 self.DVGeo.writeTecplot("deformedFFD.dat", self.nSolveAdjoints)
             else:
                 self.DVGeo.writeTecplot("deformedFFD.dat", counter)
+        """
 
     def writeTotalDeriv(self, fileName, sens, evalFuncs):
         """
