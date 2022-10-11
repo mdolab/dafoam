Fixed a critical bug in DAFoamFunctions that produced wrong gradients for cases one needs to combine two or more objectives. (#368)

tests/runTests_MphysAero.py
@@ -17,47 +17,30 @@ from pygeo import geo_utils
 
 gcomm = MPI.COMM_WORLD
 
-os.chdir("./input/Wing")
+os.chdir("./input/NACA0012")
 if gcomm.rank == 0:
     os.system("rm -rf 0 processor*")
     os.system("cp -r 0.incompressible 0")
+    os.system("cp -r system.incompressible system")
+    os.system("cp -r constant/turbulenceProperties.safv3 constant/turbulenceProperties")
 
 # aero setup
 U0 = 10.0
 p0 = 0.0
-nuTilda0 = 4.5e-5
-CL_target = 0.2
-pitch0 = 2.0
-rho0 = 1.0
-A0 = 45.5
+A0 = 0.1
+aoa0 = 3.0
+LRef = 1.0
 
 daOptions = {
     "designSurfaces": ["wing"],
     "solverName": "DASimpleFoam",
     "primalMinResTol": 1.0e-10,
+    "primalMinResTolDiff": 1e4,
     "primalBC": {
         "U0": {"variable": "U", "patches": ["inout"], "value": [U0, 0.0, 0.0]},
         "p0": {"variable": "p", "patches": ["inout"], "value": [p0]},
-        "nuTilda0": {"variable": "nuTilda", "patches": ["inout"], "value": [nuTilda0]},
         "useWallFunction": True,
-    },
-    "fvSource": {
-        "disk1": {
-            "type": "actuatorDisk",
-            "source": "cylinderAnnulusSmooth",
-            "center": [-1.0, 0.0, 5.0],
-            "direction": [1.0, 0.0, 0.0],
-            "innerRadius": 0.5,
-            "outerRadius": 5.0,
-            "rotDir": "right",
-            "scale": 1.0,
-            "POD": 0.0,
-            "eps": 3.0,  # eps should be of cell size
-            "expM": 1.0,
-            "expN": 0.5,
-            "adjustThrust": 1,
-            "targetThrust": 100.0,
-        },
+        "transport:nu": 1.5e-5,
     },
     "objFunc": {
         "CD": {
@@ -65,9 +48,9 @@ daOptions = {
                 "type": "force",
                 "source": "patchToFace",
                 "patches": ["wing"],
-                "directionMode": "fixedDirection",
-                "direction": [1.0, 0.0, 0.0],
-                "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+                "directionMode": "parallelToFlow",
+                "alphaName": "aoa",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0),
                 "addToAdjoint": True,
             }
         },
@@ -76,22 +59,29 @@ daOptions = {
                 "type": "force",
                 "source": "patchToFace",
                 "patches": ["wing"],
-                "directionMode": "fixedDirection",
-                "direction": [0.0, 1.0, 0.0],
-                "scale": 1.0 / (0.5 * U0 * U0 * A0 * rho0),
+                "directionMode": "normalToFlow",
+                "alphaName": "aoa",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0),
                 "addToAdjoint": True,
             }
         },
-        "CMZ": {
-            "part1": {
-                "type": "moment",
-                "source": "patchToFace",
-                "patches": ["wing"],
-                "axis": [0.0, 0.0, 1.0],
-                "center": [0.0, 0.0, 0.0],
-                "scale": 1.0 / (0.5 * U0 * U0 * A0 * 1.0),
-                "addToAdjoint": True,
-            }
+    },
+    "fvSource": {
+        "disk1": {
+            "type": "actuatorDisk",
+            "source": "cylinderAnnulusSmooth",
+            "center": [-0.55, 0.0, 0.05],
+            "direction": [1.0, 0.0, 0.0],
+            "innerRadius": 0.01,
+            "outerRadius": 0.4,
+            "rotDir": "right",
+            "scale": 100.0,
+            "POD": 0.0,
+            "eps": 0.1,  # eps should be of cell size
+            "expM": 1.0,
+            "expN": 0.5,
+            "adjustThrust": 0,
+            "targetThrust": 1.0,
         },
     },
     "adjEqnOption": {
@@ -99,34 +89,22 @@ daOptions = {
         "pcFillLevel": 1,
         "jacMatReOrdering": "rcm",
     },
-    "normalizeStates": {
-        "U": U0,
-        "p": U0 * U0 / 2.0,
-        "nuTilda": 1e-3,
-        "phi": 1.0,
-    },
+    "normalizeStates": {"U": U0, "p": U0 * U0 / 2.0, "phi": 1.0, "nuTilda": 1e-3},
     "adjPartDerivFDStep": {"State": 1e-6},
-    "checkMeshThreshold": {
-        "maxAspectRatio": 5000.0,
-        "maxNonOrth": 70.0,
-        "maxSkewness": 8.0,
-        "maxIncorrectlyOrientedFaces": 0,
-    },
     "designVar": {
-        "twist": {"designVarType": "FFD"},
         "shape": {"designVarType": "FFD"},
-        "actuator": {"designVarType": "ACTD", "actuatorName": "disk1"},
+        "aoa": {"designVarType": "AOA", "patches": ["inout"], "flowAxis": "x", "normalAxis": "y"},
+        "actuator_radius": {"designVarType": "ACTD", "actuatorName": "disk1", "comps": [4]},
+        "actuator_center": {"designVarType": "ACTD", "actuatorName": "disk1", "comps": [0, 1, 2]},
         "uin": {"designVarType": "BC", "patches": ["inout"], "variable": "U", "comp": 0},
     },
-    "adjPCLag": 1,
 }
 
 meshOptions = {
     "gridFile": os.getcwd(),
     "fileType": "OpenFOAM",
-    "useRotations": False,
     # point and normal for the symmetry plane
-    "symmetryPlanes": [[[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]]],
+    "symmetryPlanes": [[[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]], [[0.0, 0.0, 0.1], [0.0, 0.0, 1.0]]],
 }
 
 
@@ -153,8 +131,9 @@ class Top(Multipoint):
         self.connect("mesh.x_aero0", "geometry.x_aero_in")
         self.connect("geometry.x_aero0", "cruise.x_aero")
 
+        self.add_subsystem("LoD", om.ExecComp("val=CL/CD"))
+
     def configure(self):
-        super().configure()
 
         self.cruise.aero_post.mphys_add_funcs()
 
@@ -164,60 +143,47 @@ class Top(Multipoint):
         # add pointset
         self.geometry.nom_add_discipline_coords("aero", points)
 
-        # create constraint DV setup
-        tri_points = self.mesh.mphys_get_triangulated_surface()
-        self.geometry.nom_setConstraintSurface(tri_points)
-
         # geometry setup
 
         # Create reference axis
-        nRefAxPts = self.geometry.nom_addRefAxis(name="wingAxis", xFraction=0.25, alignIndex="k")
+        def aoa(val, DASolver):
+            aoa = val[0] * np.pi / 180.0
+            U = [float(U0 * np.cos(aoa)), float(U0 * np.sin(aoa)), 0]
+            # we need to update the U value only
+            DASolver.setOption("primalBC", {"U0": {"value": U}})
+            DASolver.updateDAOption()
 
-        # Set up global design variables
-        def twist(val, geo):
-            for i in range(nRefAxPts):
-                geo.rot_z["wingAxis"].coef[i] = -val[i]
+        # pass this aoa function to the cruise group
+        self.cruise.coupling.solver.add_dv_func("aoa", aoa)
+        self.cruise.aero_post.add_dv_func("aoa", aoa)
+
+        def actuator_radius(val, DASolver):
+            actR2 = float(val[0])
+            # only change design variables
+            DASolver.setOption("fvSource", {"disk1": {"outerRadius": actR2}})
+            DASolver.updateDAOption()
+
+        self.cruise.coupling.solver.add_dv_func("actuator_radius", actuator_radius)
+        self.cruise.aero_post.add_dv_func("actuator_radius", actuator_radius)
+
+        def actuator_center(val, DASolver):
+            actX = float(val[0])
+            actY = float(val[1])
+            actZ = float(val[2])
+            # only change design variables
+            DASolver.setOption("fvSource", {"disk1": {"center": [actX, actY, actZ]}})
+            DASolver.updateDAOption()
+
+        self.cruise.coupling.solver.add_dv_func("actuator_center", actuator_center)
+        self.cruise.aero_post.add_dv_func("actuator_center", actuator_center)
 
         def uin(val, DASolver):
             U = [float(val[0]), 0.0, 0.0]
             DASolver.setOption("primalBC", {"U0": {"value": U}})
             DASolver.updateDAOption()
-
+        
         self.cruise.coupling.solver.add_dv_func("uin", uin)
         self.cruise.aero_post.add_dv_func("uin", uin)
-
-        def actuator(val, DASolver):
-            actX = float(val[0])
-            actY = float(val[1])
-            actZ = float(val[2])
-            actR1 = float(val[3])
-            actR2 = float(val[4])
-            actScale = float(val[5])
-            actPOD = float(val[6])
-            actExpM = float(val[7])
-            actExpN = float(val[8])
-            T = float(val[9])
-            DASolver.setOption(
-                "fvSource",
-                {
-                    "disk1": {
-                        "center": [actX, actY, actZ],
-                        "innerRadius": actR1,
-                        "outerRadius": actR2,
-                        "scale": actScale,
-                        "POD": actPOD,
-                        "expM": actExpM,
-                        "expN": actExpN,
-                        "targetThrust": T
-                    },
-                },
-            )
-            DASolver.updateDAOption()
-
-        self.cruise.coupling.solver.add_dv_func("actuator", actuator)
-        self.cruise.aero_post.add_dv_func("actuator", actuator)
-
-        self.geometry.nom_addGlobalDV(dvName="twist", value=np.array([pitch0] * nRefAxPts), func=twist)
 
         # Select all points
         pts = self.geometry.DVGeo.getLocalIndex(0)
@@ -225,55 +191,49 @@ class Top(Multipoint):
         PS = geo_utils.PointSelect("list", indexList)
         nShapes = self.geometry.nom_addLocalDV(dvName="shape", pointSelect=PS)
 
-        leList = [[0.1, 0, 0.01], [7.5, 0, 13.9]]
-        teList = [[4.9, 0, 0.01], [8.9, 0, 13.9]]
-        self.geometry.nom_addThicknessConstraints2D("thickcon", leList, teList, nSpan=10, nChord=10)
-        self.geometry.nom_addVolumeConstraint("volcon", leList, teList, nSpan=10, nChord=10)
-        self.geometry.nom_add_LETEConstraint("lecon", 0, "iLow")
-        self.geometry.nom_add_LETEConstraint("tecon", 0, "iHigh")
-        # self.geometry.nom_addCurvatureConstraint1D(
-        #    "curvature", start=[3, 0, 0], end=[8, 0, 12], nPts=20, axis=[0, 1, 0], curvatureType="mean", scaled=False
-        # )
-        self.geometry.nom_addLERadiusConstraints(
-            "radius", leList=[[0.1, 0, 0], [7.0, 0, 13]], nSpan=10, axis=[0, 1, 0], chordDir=[1, 0, 0]
-        )
-
         # add dvs to ivc and connect
-        self.dvs.add_output("twist", val=np.array([pitch0] * nRefAxPts))
         self.dvs.add_output("shape", val=np.array([0] * nShapes))
+        self.dvs.add_output("aoa", val=np.array([aoa0]))
         self.dvs.add_output("uin", val=np.array([U0]))
-        self.dvs.add_output("actuator", val=np.array([-1, 0.0, 5, 0.5, 5.0, 1.0, 1.0, 1.0, 0.5, 100.0]))
-        self.connect("twist", "geometry.twist")
+        self.dvs.add_output("actuator_radius", val=np.array([0.4]))
+        self.dvs.add_output("actuator_center", val=np.array([-0.55, 0, 0.05]))
+
         self.connect("shape", "geometry.shape")
+        self.connect("aoa", "cruise.aoa")
         self.connect("uin", "cruise.uin")
-        self.connect("actuator", "cruise.actuator")
+        self.connect("actuator_radius", "cruise.actuator_radius")
+        self.connect("actuator_center", "cruise.actuator_center")
 
         # define the design variables
-        self.add_design_var("twist", lower=-10.0, upper=10.0, scaler=1.0)
         self.add_design_var("shape", lower=-1.0, upper=1.0, scaler=1.0)
+        self.add_design_var("aoa", lower=-10.0, upper=10.0, scaler=1.0)
         self.add_design_var("uin", lower=9.0, upper=11.0, scaler=1.0)
-        self.add_design_var("actuator", lower=-1000.0, upper=1000.0, scaler=1.0)
+        self.add_design_var("actuator_radius", lower=-1000.0, upper=1000.0, scaler=1.0)
+        self.add_design_var("actuator_center", lower=-1000.0, upper=1000.0, scaler=1.0)
 
         # add constraints and the objective
-        self.add_objective("cruise.aero_post.CD", scaler=1.0)
-        self.add_constraint("cruise.aero_post.CL", equals=0.3, scaler=1.0)
-        self.add_constraint("geometry.thickcon", lower=0.5, upper=3.0, scaler=1.0)
-        self.add_constraint("geometry.volcon", lower=1.0, scaler=1.0)
-        self.add_constraint("geometry.tecon", equals=0.0, scaler=1.0, linear=True)
-        self.add_constraint("geometry.lecon", equals=0.0, scaler=1.0, linear=True)
-        # self.add_constraint("geometry.curvature", lower=0.0, upper=0.02, scaler=1.0)
-        self.add_constraint("geometry.radius", lower=1.0, scaler=1.0)
+        self.connect("cruise.aero_post.CD", "LoD.CD")
+        self.connect("cruise.aero_post.CL", "LoD.CL")
+        self.add_objective("LoD.val", scaler=1.0)
 
 
 prob = om.Problem()
 prob.model = Top()
 
 prob.driver = om.pyOptSparseDriver()
-prob.driver.options["optimizer"] = "SLSQP"
+prob.driver.options["optimizer"] = "IPOPT"
 prob.driver.opt_settings = {
-    "ACC": 1.0e-5,
-    "MAXIT": 2,
-    "IFILE": "opt_SLSQP.txt",
+    "tol": 1.0e-7,
+    "max_iter": 50,
+    "output_file": "opt_IPOPT.out",
+    "constr_viol_tol": 1.0e-7,
+    "mu_strategy": "adaptive",
+    "limited_memory_max_history": 26,
+    "nlp_scaling_method": "gradient-based",
+    "alpha_for_y": "full",
+    "recalc_y": "yes",
+    "print_level": 5,
+    "acceptable_tol": 1.0e-7,
 }
 prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
 
@@ -284,42 +244,19 @@ prob.recording_options["record_objectives"] = True
 prob.recording_options["record_constraints"] = True
 
 prob.setup(mode="rev")
-om.n2(prob, show_browser=False, outfile="mphys_aerostruct.html")
+om.n2(prob, show_browser=False, outfile="mphys_aero.html")
 
-optFuncs = OptFuncs([daOptions], prob)
+optFuncs = OptFuncs(daOptions, prob)
 
-prob.run_driver()
-
-prob.record("final")
-prob.cleanup()
-
-cr = om.CaseReader("cases.sql")
-
-# get list of cases recorded on problem
-problem_cases = cr.list_cases("problem")
-cr.list_source_vars("problem")
-case = cr.get_case("final")
-
-finalObj = case.get_objectives()
-finalCon = case.get_constraints()
-
-finalCL = {}
-for key in finalCon.keys():
-    if "CL" in key:
-        finalCL[key] = finalCon[key]
+prob.run_model()
+totals = prob.compute_totals()
 
 if gcomm.rank == 0:
-    reg_write_dict(finalObj, 1e-6, 1e-8)
-    reg_write_dict(finalCL, 1e-6, 1e-8)
-
-# test the find feasible design function
-optFuncs.findFeasibleDesign(
-    ["cruise.aero_post.CL", "cruise.aero_post.CMZ"], ["twist", "twist"], designVarsComp=[0, 1], targets=[0.35, 1.5]
-)
-CL = prob.get_val("cruise.aero_post.CL")[0]
-CMZ = prob.get_val("cruise.aero_post.CMZ")[0]
-error = np.array([CL - 0.35, CMZ - 1.5])
-error_norm = np.linalg.norm(error)
-if error_norm > 2e-4:
-    print("findFeasibleDesign Failed!!!")
-    exit(1)
+    derivDict = {}
+    derivDict["LoD.val"] = {}
+    derivDict["LoD.val"]["shape"] = totals[("LoD.val", "dvs.shape")][0]
+    derivDict["LoD.val"]["aoa"] = totals[("LoD.val", "dvs.aoa")][0]
+    derivDict["LoD.val"]["uin"] = totals[("LoD.val", "dvs.uin")][0]
+    derivDict["LoD.val"]["actuator_radius"] = totals[("LoD.val", "dvs.actuator_radius")][0]
+    derivDict["LoD.val"]["actuator_center"] = totals[("LoD.val", "dvs.actuator_center")][0]
+    reg_write_dict(derivDict, 1e-4, 1e-6)