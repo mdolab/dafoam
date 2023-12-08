82f1f585108d8363c240cbdf7c6c282c0b9747f6
Ping He <phe@iastate.edu>
2023-12-08 10:34:14 -0600

    Fixed a potential issue for area calculation in a few objFuncs.

--- src/adjoint/DAObjFunc/DAObjFuncWallHeatFlux.C
+++ src/adjoint/DAObjFunc/DAObjFuncWallHeatFlux.C
@@ -162,20 +162,17 @@ void DAObjFuncWallHeatFlux::calcObjFunc(
         objFuncValue: the sum of objective, reduced across all processors and scaled by "scale"
     */
 
-    // calculate the area of all the heat flux patches
-    if (areaSum_ < 0.0)
+    // always calculate the area of all the heat flux patches
+    areaSum_ = 0.0;
+    forAll(objFuncFaceSources, idxI)
     {
-        areaSum_ = 0.0;
-        forAll(objFuncFaceSources, idxI)
-        {
-            const label& objFuncFaceI = objFuncFaceSources[idxI];
-            label bFaceI = objFuncFaceI - daIndex_.nLocalInternalFaces;
-            const label patchI = daIndex_.bFacePatchI[bFaceI];
-            const label faceI = daIndex_.bFaceFaceI[bFaceI];
-            areaSum_ += mesh_.magSf().boundaryField()[patchI][faceI];
-        }
-        reduce(areaSum_, sumOp<scalar>());
+        const label& objFuncFaceI = objFuncFaceSources[idxI];
+        label bFaceI = objFuncFaceI - daIndex_.nLocalInternalFaces;
+        const label patchI = daIndex_.bFacePatchI[bFaceI];
+        const label faceI = daIndex_.bFaceFaceI[bFaceI];
+        areaSum_ += mesh_.magSf().boundaryField()[patchI][faceI];
     }
+    reduce(areaSum_, sumOp<scalar>());
 
     // initialize faceValues to zero
     forAll(objFuncFaceValues, idxI)
