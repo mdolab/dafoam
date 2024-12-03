cb0d09156fb6680a1fd446a2740d3836106aa1da
Ping He <phe@iastate.edu>
2024-12-03 14:08:38 -0600

    Changed init file.

--- dafoam/__init__.py
+++ dafoam/__init__.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
 
-# from .pyDAFoam import PYDAFOAM
-# from . import optFuncs
-from . import pyUnitTests
+from .pyDAFoam import PYDAFOAM
+from . import optFuncs
+#from . import pyUnitTests
