a0adb0f86199b285fc9eada2d5651be68274619a
Chris Psenica <cpsenica@Brahms.local>
2025-03-03 14:35:53 -0600

    bug fixes

--- src/adjoint/DAInput/DAInputThermalCoupling.C
+++ src/adjoint/DAInput/DAInputThermalCoupling.C
@@ -6,7 +6,6 @@
 \*---------------------------------------------------------------------------*/
 
 #include "DAInputThermalCoupling.H"
-#include "DAFunction.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -39,7 +38,7 @@ DAInputThermalCoupling::DAInputThermalCoupling(
 
     // check and assign values for discipline and formulation
     discipline_ = daOption_.getAllOptions().getWord("discipline");
-    word functionName = getFunctionName();
+    word functionName = daFunction.getFunctionName();
     formMode_ = daOption_.getAllOptions().subDict("function").subDict(functionName).lookupOrDefault<word>("formulation", "default");
 
     size_ = 0;
