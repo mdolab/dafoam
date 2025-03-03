559745c73022f908d4e71100b450e7d9a3a2e6d6
Chris Psenica <cpsenica@Brahms.local>
2025-03-03 15:58:17 -0600

    bug fixes

--- src/adjoint/DAInput/DAInputThermalCoupling.C
+++ src/adjoint/DAInput/DAInputThermalCoupling.C
@@ -67,197 +67,70 @@ DAInputThermalCoupling::DAInputThermalCoupling(
     }
     // we have two sets of variable to transfer
     size_ *= 2;
+}
 
-    void DAInputThermalCoupling::run(const scalarList& input)
+void DAInputThermalCoupling::run(const scalarList& input)
+{
+    /*
+Description:
+    Assign the input to OF fields
+*/
+
+    volScalarField& T =
+        const_cast<volScalarField&>(mesh_.thisDb().lookupObject<volScalarField>("T"));
+
+    // ********* first loop, set the refValue
+    label counterI = 0;
+    forAll(patches_, idxI)
     {
-        /*
-    Description:
-        Assign the input to OF fields
-    */
+        // get the patch id label
+        word patchName = patches_[idxI];
+        label patchI = mesh_.boundaryMesh().findPatchID(patchName);
 
-        volScalarField& T =
-            const_cast<volScalarField&>(mesh_.thisDb().lookupObject<volScalarField>("T"));
+        mixedFvPatchField<scalar>& mixedPatch =
+            refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
 
-        // ********* first loop, set the refValue
-        label counterI = 0;
-        forAll(patches_, idxI)
+        forAll(mixedPatch.refValue(), faceI)
         {
-            // get the patch id label
-            word patchName = patches_[idxI];
-            label patchI = mesh_.boundaryMesh().findPatchID(patchName);
-
-            mixedFvPatchField<scalar>& mixedPatch =
-                refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
-
-            forAll(mixedPatch.refValue(), faceI)
-            {
-                mixedPatch.refValue()[faceI] = input[counterI];
-                mixedPatch.refGrad()[faceI] = 0;
-                counterI++;
-            }
+            mixedPatch.refValue()[faceI] = input[counterI];
+            mixedPatch.refGrad()[faceI] = 0;
+            counterI++;
         }
+    }
 
-        // ********* second loop, set the valueFraction:
-        // neighKDeltaCoeffs / ( neighKDeltaCoeffs + myKDeltaCoeffs)
-        scalar deltaCoeffs = 0;
+    // ********* second loop, set the valueFraction:
+    // neighKDeltaCoeffs / ( neighKDeltaCoeffs + myKDeltaCoeffs)
+    scalar deltaCoeffs = 0;
 
-        if (discipline_ == "aero")
+    if (discipline_ == "aero")
+    {
+
+        // for incompressible flow  Q = Cp * alphaEff * dT/dz, so kappa = Cp * alphaEff
+        DATurbulenceModel& daTurb = const_cast<DATurbulenceModel&>(daModel_.getDATurbulenceModel());
+        word turbModelType = daTurb.getTurbModelType();
+
+        if (turbModelType == "incompressible")
         {
+            volScalarField alphaEff = daTurb.alphaEff();
 
-            // for incompressible flow  Q = Cp * alphaEff * dT/dz, so kappa = Cp * alphaEff
-            DATurbulenceModel& daTurb = const_cast<DATurbulenceModel&>(daModel_.getDATurbulenceModel());
-            word turbModelType = daTurb.getTurbModelType();
-
-            if (turbModelType == "incompressible")
-            {
-                volScalarField alphaEff = daTurb.alphaEff();
-
-                IOdictionary transportProperties(
-                    IOobject(
-                        "transportProperties",
-                        mesh_.time().constant(),
-                        mesh_,
-                        IOobject::MUST_READ,
-                        IOobject::NO_WRITE,
-                        false));
-                scalar Cp = readScalar(transportProperties.lookup("Cp"));
-
-                forAll(patches_, idxI)
-                {
-                    // get the patch id label
-                    word patchName = patches_[idxI];
-                    label patchI = mesh_.boundaryMesh().findPatchID(patchName);
-
-                    mixedFvPatchField<scalar>& mixedPatch =
-                        refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
-
-                    forAll(mesh_.boundaryMesh()[patchI], faceI)
-                    {
-                        if (formMode_ == "default")
-                        {
-                            // deltaCoeffs = 1 / d
-                            deltaCoeffs = T.boundaryField()[patchI].patch().deltaCoeffs()[faceI];
-                        }
-                        else if (formMode_ == "daCustom")
-                        {
-                            label nearWallCellIndex = mesh_.boundaryMesh()[patchI].faceCells()[faceI];
-                            vector c1 = mesh_.Cf().boundaryField()[patchI][faceI];
-                            vector c2 = mesh_.C()[nearWallCellIndex];
-                            scalar d = mag(c1 - c2);
-                            deltaCoeffs = 1 / d;
-                        }
-                        else
-                        {
-                            FatalErrorIn(" ") << "formulation: "
-                                              << formMode_ << " not supported!"
-                                              << " Options are: default and daCustom."
-                                              << abort(FatalError);
-                        }
-                        scalar alphaEffBf = alphaEff.boundaryField()[patchI][faceI];
-                        scalar myKDeltaCoeffs = Cp * alphaEffBf * deltaCoeffs;
-                        // NOTE: we continue to use the counterI from the first loop
-                        scalar neighKDeltaCoeffs = input[counterI];
-                        mixedPatch.valueFraction()[faceI] = neighKDeltaCoeffs / (myKDeltaCoeffs + neighKDeltaCoeffs);
-                        counterI++;
-                    }
-                }
-            }
-
-            if (turbModelType == "compressible")
-            {
-                // for compressible flow Q = alphaEff * dHE/dz, so if enthalpy is used, kappa = Cp * alphaEff
-                // if the internalEnergy is used, kappa = (Cp - R) * alphaEff
-                volScalarField alphaEff = daTurb.alphaEff();
-                // compressible flow, H = alphaEff * dHE/dz
-                const fluidThermo& thermo = mesh_.thisDb().lookupObject<fluidThermo>("thermophysicalProperties");
-                const volScalarField& he = thermo.he();
-
-                const IOdictionary& thermoDict = mesh_.thisDb().lookupObject<IOdictionary>("thermophysicalProperties");
-                dictionary mixSubDict = thermoDict.subDict("mixture");
-                dictionary specieSubDict = mixSubDict.subDict("specie");
-                scalar molWeight = specieSubDict.getScalar("molWeight");
-                dictionary thermodynamicsSubDict = mixSubDict.subDict("thermodynamics");
-                scalar Cp = thermodynamicsSubDict.getScalar("Cp");
-
-                // 8314.4700665  gas constant in OpenFOAM
-                // src/OpenFOAM/global/constants/thermodynamic/thermodynamicConstants.H
-                scalar RR = Foam::constant::thermodynamic::RR;
-
-                // R = RR/molWeight
-                // Foam::specie::R() function in src/thermophysicalModels/specie/specie/specieI.H
-                scalar R = RR / molWeight;
-
-                scalar tmpVal = 0;
-                // e = (Cp - R) * T, so Q = alphaEff * (Cp-R) * dT/dz
-                if (he.name() == "e")
-                {
-                    tmpVal = Cp - R;
-                }
-                // h = Cp * T, so Q = alphaEff * Cp * dT/dz
-                else
-                {
-                    tmpVal = Cp;
-                }
-
-                forAll(patches_, idxI)
-                {
-                    // get the patch id label
-                    word patchName = patches_[idxI];
-                    label patchI = mesh_.boundaryMesh().findPatchID(patchName);
-
-                    mixedFvPatchField<scalar>& mixedPatch =
-                        refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
-
-                    forAll(mesh_.boundaryMesh()[patchI], faceI)
-                    {
-                        if (formMode_ == "default")
-                        {
-                            // deltaCoeffs = 1 / d
-                            deltaCoeffs = T.boundaryField()[patchI].patch().deltaCoeffs()[faceI];
-                        }
-                        else if (formMode_ == "daCustom")
-                        {
-                            label nearWallCellIndex = mesh_.boundaryMesh()[patchI].faceCells()[faceI];
-                            vector c1 = mesh_.Cf().boundaryField()[patchI][faceI];
-                            vector c2 = mesh_.C()[nearWallCellIndex];
-                            scalar d = mag(c1 - c2);
-                            deltaCoeffs = 1 / d;
-                        }
-                        else
-                        {
-                            FatalErrorIn(" ") << "formulation: "
-                                              << formMode_ << " not supported!"
-                                              << " Options are: default and daCustom."
-                                              << abort(FatalError);
-                        }
-                        scalar alphaEffBf = alphaEff.boundaryField()[patchI][faceI];
-                        scalar myKDeltaCoeffs = tmpVal * alphaEffBf * deltaCoeffs;
-                        // NOTE: we continue to use the counterI from the first loop
-                        scalar neighKDeltaCoeffs = input[counterI];
-                        mixedPatch.valueFraction()[faceI] = neighKDeltaCoeffs / (myKDeltaCoeffs + neighKDeltaCoeffs);
-                        counterI++;
-                    }
-                }
-            }
-        }
-        else if (discipline_ == "thermal")
-        {
-            // for solid solvers Q = k * dT/dz, so kappa = k
-            IOdictionary solidProperties(
+            IOdictionary transportProperties(
                 IOobject(
-                    "solidProperties",
+                    "transportProperties",
                     mesh_.time().constant(),
                     mesh_,
                     IOobject::MUST_READ,
                     IOobject::NO_WRITE,
                     false));
-            scalar k = readScalar(solidProperties.lookup("k"));
+            scalar Cp = readScalar(transportProperties.lookup("Cp"));
 
             forAll(patches_, idxI)
             {
                 // get the patch id label
                 word patchName = patches_[idxI];
                 label patchI = mesh_.boundaryMesh().findPatchID(patchName);
+
+                mixedFvPatchField<scalar>& mixedPatch =
+                    refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
 
                 forAll(mesh_.boundaryMesh()[patchI], faceI)
                 {
@@ -277,12 +150,12 @@ DAInputThermalCoupling::DAInputThermalCoupling(
                     else
                     {
                         FatalErrorIn(" ") << "formulation: "
-                                          << formMode_ << " not supported!"
-                                          << " Options are: default and daCustom."
-                                          << abort(FatalError);
+                                            << formMode_ << " not supported!"
+                                            << " Options are: default and daCustom."
+                                            << abort(FatalError);
                     }
-                    mixedFvPatchField<scalar>& mixedPatch = refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
-                    scalar myKDeltaCoeffs = k * deltaCoeffs;
+                    scalar alphaEffBf = alphaEff.boundaryField()[patchI][faceI];
+                    scalar myKDeltaCoeffs = Cp * alphaEffBf * deltaCoeffs;
                     // NOTE: we continue to use the counterI from the first loop
                     scalar neighKDeltaCoeffs = input[counterI];
                     mixedPatch.valueFraction()[faceI] = neighKDeltaCoeffs / (myKDeltaCoeffs + neighKDeltaCoeffs);
@@ -290,14 +163,142 @@ DAInputThermalCoupling::DAInputThermalCoupling(
                 }
             }
         }
-        else
-        {
-            FatalErrorIn("DAInputThermalCoupling::run") << " discipline not valid! "
-                                                        << abort(FatalError);
-        }
 
-        T.correctBoundaryConditions();
+        if (turbModelType == "compressible")
+        {
+            // for compressible flow Q = alphaEff * dHE/dz, so if enthalpy is used, kappa = Cp * alphaEff
+            // if the internalEnergy is used, kappa = (Cp - R) * alphaEff
+            volScalarField alphaEff = daTurb.alphaEff();
+            // compressible flow, H = alphaEff * dHE/dz
+            const fluidThermo& thermo = mesh_.thisDb().lookupObject<fluidThermo>("thermophysicalProperties");
+            const volScalarField& he = thermo.he();
+
+            const IOdictionary& thermoDict = mesh_.thisDb().lookupObject<IOdictionary>("thermophysicalProperties");
+            dictionary mixSubDict = thermoDict.subDict("mixture");
+            dictionary specieSubDict = mixSubDict.subDict("specie");
+            scalar molWeight = specieSubDict.getScalar("molWeight");
+            dictionary thermodynamicsSubDict = mixSubDict.subDict("thermodynamics");
+            scalar Cp = thermodynamicsSubDict.getScalar("Cp");
+
+            // 8314.4700665  gas constant in OpenFOAM
+            // src/OpenFOAM/global/constants/thermodynamic/thermodynamicConstants.H
+            scalar RR = Foam::constant::thermodynamic::RR;
+
+            // R = RR/molWeight
+            // Foam::specie::R() function in src/thermophysicalModels/specie/specie/specieI.H
+            scalar R = RR / molWeight;
+
+            scalar tmpVal = 0;
+            // e = (Cp - R) * T, so Q = alphaEff * (Cp-R) * dT/dz
+            if (he.name() == "e")
+            {
+                tmpVal = Cp - R;
+            }
+            // h = Cp * T, so Q = alphaEff * Cp * dT/dz
+            else
+            {
+                tmpVal = Cp;
+            }
+
+            forAll(patches_, idxI)
+            {
+                // get the patch id label
+                word patchName = patches_[idxI];
+                label patchI = mesh_.boundaryMesh().findPatchID(patchName);
+
+                mixedFvPatchField<scalar>& mixedPatch =
+                    refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
+
+                forAll(mesh_.boundaryMesh()[patchI], faceI)
+                {
+                    if (formMode_ == "default")
+                    {
+                        // deltaCoeffs = 1 / d
+                        deltaCoeffs = T.boundaryField()[patchI].patch().deltaCoeffs()[faceI];
+                    }
+                    else if (formMode_ == "daCustom")
+                    {
+                        label nearWallCellIndex = mesh_.boundaryMesh()[patchI].faceCells()[faceI];
+                        vector c1 = mesh_.Cf().boundaryField()[patchI][faceI];
+                        vector c2 = mesh_.C()[nearWallCellIndex];
+                        scalar d = mag(c1 - c2);
+                        deltaCoeffs = 1 / d;
+                    }
+                    else
+                    {
+                        FatalErrorIn(" ") << "formulation: "
+                                            << formMode_ << " not supported!"
+                                            << " Options are: default and daCustom."
+                                            << abort(FatalError);
+                    }
+                    scalar alphaEffBf = alphaEff.boundaryField()[patchI][faceI];
+                    scalar myKDeltaCoeffs = tmpVal * alphaEffBf * deltaCoeffs;
+                    // NOTE: we continue to use the counterI from the first loop
+                    scalar neighKDeltaCoeffs = input[counterI];
+                    mixedPatch.valueFraction()[faceI] = neighKDeltaCoeffs / (myKDeltaCoeffs + neighKDeltaCoeffs);
+                    counterI++;
+                }
+            }
+        }
     }
+    else if (discipline_ == "thermal")
+    {
+        // for solid solvers Q = k * dT/dz, so kappa = k
+        IOdictionary solidProperties(
+            IOobject(
+                "solidProperties",
+                mesh_.time().constant(),
+                mesh_,
+                IOobject::MUST_READ,
+                IOobject::NO_WRITE,
+                false));
+        scalar k = readScalar(solidProperties.lookup("k"));
+
+        forAll(patches_, idxI)
+        {
+            // get the patch id label
+            word patchName = patches_[idxI];
+            label patchI = mesh_.boundaryMesh().findPatchID(patchName);
+
+            forAll(mesh_.boundaryMesh()[patchI], faceI)
+            {
+                if (formMode_ == "default")
+                {
+                    // deltaCoeffs = 1 / d
+                    deltaCoeffs = T.boundaryField()[patchI].patch().deltaCoeffs()[faceI];
+                }
+                else if (formMode_ == "daCustom")
+                {
+                    label nearWallCellIndex = mesh_.boundaryMesh()[patchI].faceCells()[faceI];
+                    vector c1 = mesh_.Cf().boundaryField()[patchI][faceI];
+                    vector c2 = mesh_.C()[nearWallCellIndex];
+                    scalar d = mag(c1 - c2);
+                    deltaCoeffs = 1 / d;
+                }
+                else
+                {
+                    FatalErrorIn(" ") << "formulation: "
+                                        << formMode_ << " not supported!"
+                                        << " Options are: default and daCustom."
+                                        << abort(FatalError);
+                }
+                mixedFvPatchField<scalar>& mixedPatch = refCast<mixedFvPatchField<scalar>>(T.boundaryFieldRef()[patchI]);
+                scalar myKDeltaCoeffs = k * deltaCoeffs;
+                // NOTE: we continue to use the counterI from the first loop
+                scalar neighKDeltaCoeffs = input[counterI];
+                mixedPatch.valueFraction()[faceI] = neighKDeltaCoeffs / (myKDeltaCoeffs + neighKDeltaCoeffs);
+                counterI++;
+            }
+        }
+    }
+    else
+    {
+        FatalErrorIn("DAInputThermalCoupling::run") << " discipline not valid! "
+                                                    << abort(FatalError);
+    }
+
+    T.correctBoundaryConditions();
+}
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
