0b9018fe3e6febef681dd91700c3820c29451899
Lean Fang <leanfang@iastate.edu>
2025-03-05 09:11:30 -0600

    Irk pimple (#782)
    * IrkPimple
    
    * format
    
    * relaxStage = 0.8
    
    * IRKDict optional
    
    * added IRK pimple reg test (laminar)
    
    * reg test UNorm ref updated
    
    * unitTest

---
+++ tests/runUnitTests_DAIrkPimpleFoam.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Run Python tests for optimization integration
+"""
+
+from mpi4py import MPI
+from dafoam import PYDAFOAM
+import os
+import numpy as np
+from pyofm import PYOFM
+
+gcomm = MPI.COMM_WORLD
+
+os.chdir("./reg_test_files-main/NACA0012UnsteadyV4")
+if gcomm.rank == 0:
+    os.system("rm -rf processor* *.bin")
+
+# aero setup
+U0 = 10.0
+
+daOptions = {
+    "solverName": "DAIrkPimpleFoam",
+}
+
+DASolver = PYDAFOAM(options=daOptions, comm=gcomm)
+DASolver()
+
+# read the U field at  2 and verify its norm
+nLocalCells = DASolver.solver.getNLocalCells()
+U = np.zeros(nLocalCells * 3)
+ofm = PYOFM(gcomm)
+ofm.readField("U", "volVectorField", "0.005", U)
+UNorm = np.linalg.norm(U)
+UNorm = gcomm.allreduce(UNorm, op=MPI.SUM)
+print("UNorm", UNorm)
+
+if abs(1174.8898926089107 - UNorm) / 1174.8898926089107 > 1e-6:
+    print("DAIrkPimpleFoam test failed!")
+    exit(1)
+else:
+    print("DAIrkPimpleFoam test passed!")
