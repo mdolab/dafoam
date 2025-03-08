416c513f89e0bd5f55b26908e42825ae2c8a6d6e
Fang <leanfang@iastate.edu>
2025-03-07 22:49:43 -0600

    SA-fv3

--- src/adjoint/DASolver/DAIrkPimpleFoam/DAIrkPimpleFoam.C
+++ src/adjoint/DASolver/DAIrkPimpleFoam/DAIrkPimpleFoam.C
@@ -41,10 +41,33 @@ addToRunTimeSelectionTable(DASolver, DAIrkPimpleFoam, dictionary);
 DAIrkPimpleFoam::DAIrkPimpleFoam(
     char* argsAll,
     PyObject* pyOptions)
-    : DASolver(argsAll, pyOptions)
+    : DASolver(argsAll, pyOptions),
+      // Radau23 coefficients and weights
+      D10(-2),
+      D11(3.0 / 2),
+      D12(1.0 / 2),
+      D20(2),
+      D21(-9.0 / 2),
+      D22(5.0 / 2),
+      w1(3.0 / 4),
+      w2(1.0 / 4),
+
+      // SA-fv3 model coefficients
+      sigmaNut(0.66666),
+      kappa(0.41),
+      Cb1(0.1355),
+      Cb2(0.622),
+      Cw1(Cb1 / sqr(kappa) + (1.0 + Cb2) / sigmaNut),
+      Cw2(0.3),
+      Cw3(2.0),
+      Cv1(7.1),
+      Cv2(5.0)
 {
 }
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+// Functions for SA-fv3 model
+#include "mySAModel.H"
 
 void DAIrkPimpleFoam::initSolver()
 {
@@ -76,23 +99,16 @@ label DAIrkPimpleFoam::solvePrimal()
     // Turbulence disabled
     //turbulence->validate();
 
-    // Get nu from memory
+    // Get nu, nut, nuTilda, y
     volScalarField& nu = const_cast<volScalarField&>(mesh.thisDb().lookupObject<volScalarField>("nu"));
+    volScalarField& nut = const_cast<volScalarField&>(mesh.thisDb().lookupObject<volScalarField>("nut"));
+    volScalarField& nuTilda = const_cast<volScalarField&>(mesh.thisDb().lookupObject<volScalarField>("nuTilda"));
+    volScalarField& y = const_cast<volScalarField&>(mesh.thisDb().lookupObject<volScalarField>("yWall"));
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
     Info << "\nStarting time loop\n"
          << endl;
-
-    // Radau23 coefficients and weights
-    scalar D10 = -2;
-    scalar D11 = 3.0 / 2;
-    scalar D12 = 1.0 / 2;
-    scalar D20 = 2;
-    scalar D21 = -9.0 / 2;
-    scalar D22 = 5.0 / 2;
-    scalar w1 = 3.0 / 4;
-    scalar w2 = 1.0 / 4;
 
     // get IRKDict settings, default to Radau23 for now
     IOdictionary IRKDict(
@@ -130,6 +146,15 @@ label DAIrkPimpleFoam::solvePrimal()
         }
     }
 
+    scalar relaxNuTilda = 1.0;
+    if (IRKDict.found("relaxNuTilda"))
+    {
+        if (IRKDict.getScalar("relaxNuTilda") > 0)
+        {
+            relaxNuTilda = IRKDict.getScalar("relaxNuTilda");
+        }
+    }
+
     scalar relaxStage1 = 0.8;
     if (IRKDict.found("relaxStage1"))
     {
@@ -149,6 +174,7 @@ label DAIrkPimpleFoam::solvePrimal()
     }
 
     scalar relaxUEqn = 1.0;
+    scalar relaxNuTildaEqn = 1.0;
 
     label maxSweep = 10;
     if (IRKDict.found("maxSweep"))
@@ -166,6 +192,11 @@ label DAIrkPimpleFoam::solvePrimal()
     volScalarField p2("p2", p);
     surfaceScalarField phi1("phi1", phi);
     surfaceScalarField phi2("phi2", phi);
+    // SA turbulence model
+    volScalarField nuTilda1("nuTilda1", nuTilda);
+    volScalarField nuTilda2("nuTilda2", nuTilda);
+    volScalarField nut1("nut1", nut);
+    volScalarField nut2("nut2", nut);
 
     // Settings for stage pressure
     mesh.setFluxRequired(p1.name());
@@ -180,20 +211,24 @@ label DAIrkPimpleFoam::solvePrimal()
     */
 
     // Initialize oldTime() for under-relaxation
-    volVectorField U1OldTime("U1OldTime", U1);
-    volVectorField U2OldTime("U2OldTime", U2);
-    volScalarField p1OldTime("p1OldTime", p1);
-    volScalarField p2OldTime("p2OldTime", p2);
-    surfaceScalarField phi1OldTime("phi1OldTime", phi1);
-    surfaceScalarField phi2OldTime("phi2OldTime", phi2);
+    U1.oldTime() = U1;
+    U2.oldTime() = U2;
+    p1.oldTime() = p1;
+    p2.oldTime() = p2;
+    phi1.oldTime() = phi1;
+    phi2.oldTime() = phi2;
+    nuTilda1.oldTime() = nuTilda1;
+    nuTilda2.oldTime() = nuTilda2;
 
     // Numerical settings
     word divUScheme = "div(phi,U)";
-    //word divGradUScheme = "div((nuEff*dev2(T(grad(U)))))";
+    word divGradUScheme = "div((nuEff*dev2(T(grad(U)))))";
+    word divNuTildaScheme = "div(phi,nuTilda)";
 
     const fvSolution& myFvSolution = mesh.thisDb().lookupObject<fvSolution>("fvSolution");
     dictionary solverDictU = myFvSolution.subDict("solvers").subDict("U");
     dictionary solverDictP = myFvSolution.subDict("solvers").subDict("p");
+    dictionary solverDictNuTilda = myFvSolution.subDict("solvers").subDict("nuTilda");
 
     while (runTime.run())
     {
@@ -219,6 +254,9 @@ label DAIrkPimpleFoam::solvePrimal()
                 {
 #include "p1EqnIrkPimple.H"
                 }
+
+                // --- Correct turbulence, using our own SAFv3
+#include "nuTilda1EqnIrkPimple.H"
             }
 
             {
@@ -228,6 +266,9 @@ label DAIrkPimpleFoam::solvePrimal()
                 {
 #include "p2EqnIrkPimple.H"
                 }
+
+                // --- Correct turbulence, using our own SAFv3
+#include "nuTilda2EqnIrkPimple.H"
             }
 
             sweepIndex++;
@@ -237,12 +278,16 @@ label DAIrkPimpleFoam::solvePrimal()
         U = U2;
         p = p2;
         phi = phi2;
+        nuTilda = nuTilda2;
+        nut = nut2;
 
         runTime.write();
         // Also write internal stages to disk (Radau23)
         U1.write();
         p1.write();
         phi1.write();
+        nuTilda1.write();
+        nut1.write();
 
         // Use old step as initial guess for the next step
         U1 = U;
@@ -250,6 +295,10 @@ label DAIrkPimpleFoam::solvePrimal()
         p1 = p;
         p1.correctBoundaryConditions();
         phi1 = phi;
+        nuTilda1 = nuTilda;
+        nuTilda1.correctBoundaryConditions();
+        nut1 = nut;
+        nut1.correctBoundaryConditions();
 
         runTime.printExecutionTime(Info);
     }
