bd277f83e001feaccb43a8e338755b783b47d959
Ping He <phe@iastate.edu>
2025-03-26 13:23:44 -0500

    Fixed more tests.

--- tests/runUnitTests_ActuatorPoint.py
+++ tests/runUnitTests_ActuatorPoint.py
@@ -105,6 +105,9 @@ daOptions = {
             "components": ["solver", "function"],
         },
     },
+    "unsteadyCompOutput": {
+        "CD": ["CD"],
+    },
 }
 
 meshOptions = {
@@ -138,7 +141,7 @@ class Top(Group):
 
         # add constraints and the objective
         self.add_objective("CD", scaler=1.0)
-        self.add_constraint("CL", equals=0.3)
+        # self.add_constraint("CL", equals=0.3)
 
 
 prob = om.Problem()
