3aefec63d06293b6713b2b5d9d4d3d32c9186fa7
Ping He <phe@iastate.edu>
2025-05-09 17:58:06 -0500

    Added a fixedWallHeatFlux BC for incomp flows (#826)
    * Added a fixedWallHeatFlux BC.
    
    * Added a test for the wallHeatFlux bc.
    
    * Fixed the format.
    
    * Added print into for alphat auto-BC.

---
+++ src/adjoint/DAMisc/fixedWallHeatFlux/fixedWallHeatFluxFvPatchScalarField.C
@@ -0,0 +1,154 @@
+/*---------------------------------------------------------------------------*\
+
+    DAFoam  : Discrete Adjoint with OpenFOAM
+    Version : v4
+
+    OpenFOAM: The Open Source CFD Toolbox
+
+    Copyright (C): 2011-2016 OpenFOAM Foundation
+
+    OpenFOAM License:
+
+        OpenFOAM is free software: you can redistribute it and/or modify it
+        under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+    
+        OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+        ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+        FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+        for more details.
+    
+        You should have received a copy of the GNU General Public License
+        along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fixedWallHeatFluxFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "volFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+fixedWallHeatFluxFvPatchScalarField::
+    fixedWallHeatFluxFvPatchScalarField(
+        const fvPatch& p,
+        const DimensionedField<scalar, volMesh>& iF)
+    : fixedGradientFvPatchScalarField(p, iF)
+{
+    fvPatchScalarField::operator=(patchInternalField());
+    gradient() = 0.0;
+}
+
+fixedWallHeatFluxFvPatchScalarField::
+    fixedWallHeatFluxFvPatchScalarField(
+        const fixedWallHeatFluxFvPatchScalarField& tdpvf,
+        const fvPatch& p,
+        const DimensionedField<scalar, volMesh>& iF,
+        const fvPatchFieldMapper& mapper)
+    : fixedGradientFvPatchScalarField(tdpvf, p, iF, mapper)
+{
+}
+
+fixedWallHeatFluxFvPatchScalarField::
+    fixedWallHeatFluxFvPatchScalarField(
+        const fvPatch& p,
+        const DimensionedField<scalar, volMesh>& iF,
+        const dictionary& dict)
+    : fixedGradientFvPatchScalarField(p, iF),
+      heatFlux_(dict.getScalar("heatFlux")),
+      nu_(dict.getScalar("nu")),
+      Pr_(dict.getScalar("Pr")),
+      Prt_(dict.getScalar("Prt")),
+      Cp_(dict.getScalar("Cp"))
+{
+    fvPatchScalarField::operator=(patchInternalField());
+    gradient() = 0.0;
+}
+
+fixedWallHeatFluxFvPatchScalarField::
+    fixedWallHeatFluxFvPatchScalarField(
+        const fixedWallHeatFluxFvPatchScalarField& tdpvf)
+    : fixedGradientFvPatchScalarField(tdpvf),
+      heatFlux_(tdpvf.heatFlux_),
+      nu_(tdpvf.nu_),
+      Pr_(tdpvf.Pr_),
+      Prt_(tdpvf.Prt_),
+      Cp_(tdpvf.Cp_)
+{
+}
+
+fixedWallHeatFluxFvPatchScalarField::
+    fixedWallHeatFluxFvPatchScalarField(
+        const fixedWallHeatFluxFvPatchScalarField& tdpvf,
+        const DimensionedField<scalar, volMesh>& iF)
+    : fixedGradientFvPatchScalarField(tdpvf, iF),
+      heatFlux_(tdpvf.heatFlux_),
+      nu_(tdpvf.nu_),
+      Pr_(tdpvf.Pr_),
+      Prt_(tdpvf.Prt_),
+      Cp_(tdpvf.Cp_)
+{
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void fixedWallHeatFluxFvPatchScalarField::autoMap(
+    const fvPatchFieldMapper& m)
+{
+
+    fixedGradientFvPatchScalarField::autoMap(m);
+}
+
+void fixedWallHeatFluxFvPatchScalarField::rmap(
+    const fvPatchScalarField& ptf,
+    const labelList& addr)
+{
+
+    fixedGradientFvPatchScalarField::rmap(ptf, addr);
+}
+
+void fixedWallHeatFluxFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    const label patchi = patch().index();
+
+    // Retrieve turbulence properties from model
+    const turbulenceModel& turbModel = db().lookupObject<turbulenceModel>(
+        IOobject::groupName(
+            turbulenceModel::propertiesName,
+            internalField().group()));
+
+    const tmp<scalarField> nutw = turbModel.nut(patchi);
+
+    gradient() = heatFlux_ / (nutw / Prt_ + nu_ / Pr_) / Cp_;
+
+    fixedGradientFvPatchScalarField::updateCoeffs();
+}
+
+void fixedWallHeatFluxFvPatchScalarField::write(Ostream& os) const
+{
+    fvPatchScalarField::write(os);
+    writeEntry("value", os);
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField(
+    fvPatchScalarField,
+    fixedWallHeatFluxFvPatchScalarField);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
