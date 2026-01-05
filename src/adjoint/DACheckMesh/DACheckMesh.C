eca8c9c7014c8831066718320fae2f5377e68606
Ping He <phe@iastate.edu>
2026-01-04 21:31:02 -0600

    Fixed the make files.

--- src/adjoint/DACheckMesh/DACheckMesh.C
+++ src/adjoint/DACheckMesh/DACheckMesh.C
@@ -13,12 +13,9 @@ namespace Foam
 {
 
 // Constructors
-DACheckMesh::DACheckMesh(
-    const DAOption& daOption,
-    const Time& runTime1,
-    const fvMesh& mesh1)
-    : daOption_(daOption),
-      runTime(runTime1),
+DACheckMesh::DACheckMesh(const fvMesh& mesh1)
+    : daOption_(mesh1.thisDb().lookupObject<DAOption>("DAOption")),
+      runTime(mesh1.time()),
       mesh(mesh1),
       surfWriter(nullptr),
       setWriter(nullptr)
@@ -41,8 +38,9 @@ DACheckMesh::DACheckMesh(
     Info << "maxIncorrectlyOrientedFaces: " << maxIncorrectlyOrientedFaces_ << endl;
 
     word surfaceFormat = "vtk";
-    surfWriter.reset(surfaceWriter::New(surfaceFormat));
-    setWriter.reset(writer<scalar>::New(vtkSetWriter<scalar>::typeName));
+    surfWriter = surfaceWriter::New(surfaceFormat);
+    setWriter = coordSetWriter::New("vtk");
+
 }
 
 DACheckMesh::~DACheckMesh()
